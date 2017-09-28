0ae6ff650bab12d46d392abc679b027b619d9654
“[Tyler <[codestack82@gmail.com]>
2017-09-27 20:51:39 -0500

    Begin working on w3 hw

---
+++ week 3/Hunt_W3_Pointers.cpp
@@ -0,0 +1,33 @@
+ /*
+* Tyler Hunt
+* Advanved C++
+* OCCC Fall 2017
+* Due: 
+*/
+
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main(){
+    cout << "\n~~~~~~~~~~~~ Pointers Calculator - Advanced C++ Week 3 ~~~~~~~~~~~~" << endl;
+
+    cout << "Chapter 7 : R7.2" << endl;
+
+
+    cout << "Chapter 7 : R7.3" << endl;
+
+
+    cout << "Chapter 7 : R7.4" << endl;
+
+
+    cout << "Chapter 7 : R7.6" << endl;
+    cout << "Chapter 7 : R7.8" << endl;
+    cout << "Chapter 7 : R7.9" << endl;
+
+    cout << "Chapter 7 : R7.10" << endl;
+
+
+    return 0;
+}
--- week 4/w4_class_code.cpp
+++ week 4/w4_class_code.cpp
@@ -8,27 +8,35 @@
 
 #include <iostream>
 #include <climits> 
+#include <cctype> //use for char checking
+#include <ctime>
 using namespace std;
 
 uint64_t fact_loop(int n);
 uint64_t fact_rec(int n);
 uint64_t fib_loop(int n);
 uint64_t fib_rec(int n);
+uint64_t fib_memo(int n);
+
+uint64_t * memo; // memo pointer array - dynamic array
 
 //main accepts int size (argc) and array of argv
 int main(int argc, const char* argv[]){
 
-    ////Read in values from the main (i.e. from the command prompt)
-    // cout << "argc is: " << argc << endl;
-    // for(int i = 0; i < argc; ++i){
-    //     cout << argv[i] << endl;
-    // }
+    // Read in values from the main (i.e. from the command prompt)
+        // cout << "argc is: " << argc << endl;
+        // for(int i = 0; i < argc; ++i){
+        //     cout << argv[i] << endl;
+        // }
+    // Note: This test show that argv[0] print your current working directory....so the first value from the
+    // command prompt is actually the second argv array value....silly, if you ask me!
 
-    int n;    
+    
+    //Note: ignore the below commented code, I was just testing different int type on my mac
+        // uint64_t x;
+        // cout << "size of uint64_t is " << sizeof(x) << endl;
 
-    // uint64_t x;
-    // cout << "size of uint64_t is " << sizeof(x) << endl;
-
+    int n;  
     if(argc == 2){         //if user gave one cmd line prompt
         n = atoi(argv[1]);
     }else{
@@ -36,13 +44,31 @@ int main(int argc, const char* argv[]){
         cin >> n;
     }
 
+    //Build timer using <ctime> library
+    clock_t start;
+    clock_t finish;
+    start = clock();        //Start timer
+
     // cout << n << " != " << fact_loop(n) << endl;
     // cout << n << " != " << fact_rec(n) << endl;
     // cout << "fib loop: " << fib_loop(n) << endl;
-    cout << "fib rec: " << fib_rec(n) << endl;
+    // cout << "fib rec: " << fib_rec(n) << endl;
+
+    memo = new uint64_t[n+1];
+
+    //initialize array with zero's 
+    for(int i = 0; i < n+1; i++){
+        memo[i] = 0;
+    } 
+
+    cout << "fib memo: " << fib_memo(n) << endl;
+
+    finish = clock();       //Stop timer
+    cout << "Elapsed time is: ~" << ((finish-start) / CLOCKS_PER_SEC) << endl;
 
     return 0;
 }
+
 //Note: replace uint64_t with __int64 in windows
 
 uint64_t fact_loop(int n){
@@ -89,10 +115,23 @@ uint64_t fib_rec(int n){
     return result;
 }
 
+uint64_t fib_memo(int n){
+    uint64_t result = 1;
 
+    if(n > 2){
+        if(memo[n] != 0){
+            result = memo[n];
+        }else{
+            result = fib_rec(n - 1) + fib_rec(n - 2);
+            memo[n] = result;               //make a note of the last fib num
+        }
+    }
+    return result;
+}
 
 /* Notes
-    - Very big numbers in c++ use unsigned Long long 
+    - Very big numbers in c++ use unsigned long long 64 bit ints (__int64 for windows) and (uint64_t for Unix) 
+        -int types have to be changed out from __int64 to uint64_t 
     - #include <climits>  <-Library where the limits and standard sizes for various types are stored
     - The factorial of 0 is 1  (0! = 1)
     - Possibly submit .exe file with future assignment if it has cmd line input params
@@ -109,4 +148,14 @@ uint64_t fib_rec(int n){
     fact_loop   Thete(n)        Theta(1)
     fact_rec    Theta(n)        Theta(n)
     fib_loop    Theta(n)        Theta(1)
+    fib_rec     Theta(2^n)      Theta(2^n)
+    fib_memo    Thete(n)        Theta(1)
+
+
+    Homework - Palindromes use recursive algorythm - preprocess ()
+        -1) figure out what the base cases are (if length is 0 or 1)
+        -2) check first and last char (then compare them)
+        -3) if true then then pop them off and continue, if false then false
+        use pointers to solve  <cctype> isalnum.....
+            basically same as the adv java hw
 */
